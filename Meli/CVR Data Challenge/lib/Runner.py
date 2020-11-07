fb8b1b56628c380276ad28a630847b4cc2e40dd8
Jonathan Munoz <jonmunm@gmail.com>
2020-11-07 17:20:30 -0300

    Mayor changes

--- Meli/CVR Data Challenge/lib/Runner.py
+++ Meli/CVR Data Challenge/lib/Runner.py
@@ -12,55 +12,6 @@ class Runner:
         self.model = model
         self.optimizer = optimizer
         self.loss_fn = loss_fn
-        
-    def find_lr(self, data_loader, init_value=1e-8, final_value=10., beta=0.98):
-        num = len(data_loader)-1
-        mult = (final_value / init_value) ** (1/num)
-        lr = init_value
-        self.optimizer.param_groups[0]['lr'] = lr
-        
-        avg_loss = 0.
-        best_loss = 0.
-        batch_num = 0
-        losses = []
-        log_lrs = []
-        
-        for x_numerical, x_categorical, y in data_loader:
-            batch_num += 1
-            
-            #As before, get the loss for this mini-batch of inputs/outputs
-            #inputs,labels = data
-            #inputs, labels = Variable(inputs), Variable(labels)
-            #optimizer.zero_grad()
-            
-            y_pred = self.model.forward(x_numerical, x_categorical)
-            loss = self.loss_fn(y_pred, y)
-            
-            #Compute the smoothed loss
-            avg_loss = beta * avg_loss + (1-beta) * loss.item()
-            smoothed_loss = avg_loss / (1 - beta**batch_num)
-            
-            #Stop if the loss is exploding
-            if batch_num > 1 and smoothed_loss > 4 * best_loss:
-                return log_lrs, losses
-            
-            #Record the best loss
-            if smoothed_loss < best_loss or batch_num==1:
-                best_loss = smoothed_loss
-                
-            #Store the values
-            losses.append(smoothed_loss)
-            log_lrs.append(math.log10(lr))
-            
-            #Do the SGD step
-            loss.backward()
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-            
-            #Update the lr for the next step
-            lr *= mult
-            self.optimizer.param_groups[0]['lr'] = lr
-        return log_lrs, losses
     
     def fit(self, data_loader, epochs, optimizer, scheduler, loss_fn):
         self.model.train()
--- Meli/CVR Data Challenge/lib/Transformers.py
+++ Meli/CVR Data Challenge/lib/Transformers.py
@@ -3,7 +3,6 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.preprocessing import FunctionTransformer as FT
 from sklearn.base import TransformerMixin
 from sklearn.impute import SimpleImputer
-from typing import List, Dict, Tuple, Union
 import numpy as np
 
 class NumericalTransformer(TransformerMixin):
@@ -31,7 +30,7 @@ class NumericalTransformer(TransformerMixin):
         self.post_tfms = post_tfms
         
     def fit_transform(self, X):
-        X = np.array(X[self.key], dtype=self.dtype)
+        X = np.array(X[self.key], dtype=self.dtype) if self.key is not None else X
         
         for tfm in self.pre_tfms:
             X = np.array(tfm.transform(X))
@@ -44,16 +43,18 @@ class NumericalTransformer(TransformerMixin):
             
         return X
     
-    def transform(self, data:np.ndarray) -> np.ndarray:       
-        for tms in self.pre_tms:
-            data = tms.transform(data)
+    def transform(self, X):     
+        X = np.array(X[self.key], dtype=self.dtype) if self.key is not None else X
+        
+        for tfm in self.pre_tfms:
+            X = np.array(tfm.transform(X))
             
-        data = self.imputer.transform(np.array(data).reshape(-1,1)).squeeze() if self.imputer is not None else data
-        data = self.scaler.transform(np.array(data).reshape(-1,1)).squeeze() if self.scaler is not None else data
+        X = self.imputer.transform(X.reshape(-1,1)).squeeze() if self.imputer is not None else X
+        X = self.scaler.transform(X.reshape(-1,1)).squeeze() if self.scaler is not None else X
         
-        for tms in self.post_tms:
-            X = tms.transform(X)        
-        
+        for tfm in self.post_tfms:
+            X = tms.transform(X)
+            
         return X
 
 class CategoricalTransformer(TransformerMixin):
@@ -73,7 +74,7 @@ class CategoricalTransformer(TransformerMixin):
         self.post_tfms = post_tfms
         
     def fit_transform(self, X, Y=None):
-        X = np.array(X[self.key], dtype=np.str)
+        X = np.array(X[self.key], dtype=np.str) if self.key is not None else X
         
         for tfm in self.pre_tfms:
             X = np.array(tfm.transform(X))
@@ -89,10 +90,10 @@ class CategoricalTransformer(TransformerMixin):
         return X
     
     def transform(self, X):      
-        X = np.array(X[self.key], dtype=np.str)
+        X = np.array(X[self.key], dtype=np.str) if self.key is not None else X
         
         for tfm in self.pre_tfms:
-            X = tfm.transform(X)
+            X = np.array(tfm.transform(X))
             
         if self.handle_unknown:
             X = np.array([x if x in self.encoder.classes_ else 'UNKNOWN' for x in X])
@@ -107,40 +108,45 @@ class CategoricalTransformer(TransformerMixin):
         return X
     
 class CategoricalTargetTransformer(TransformerMixin):
-    def __init__(self, target:str):
-        self.target = target
+    def __init__(self, key):
+        self.key = key
         self.encoder = LabelEncoder()
         
-    def fit_transform(self, data:Union[List[bool], List[str]]) -> np.ndarray:
-        data = np.array([str(item).strip() for item in data], dtype=object)
-        return self.encoder.fit_transform(data).reshape(-1,1)
+    def fit_transform(self, X):
+        X = np.array([str(x).strip() for x in X], dtype=np.str)
+        return self.encoder.fit_transform(X).reshape(-1,1)
     
-    def transform(self, data:Union[List[bool], List[str]]) -> np.ndarray:        
-        data = np.array([str(item).strip() for item in data], dtype=object)  
-        return self.encoder.transform(data).reshape(-1,1)
+    def transform(self, X):        
+        X = np.array([str(x).strip() for x in X], dtype=np.str) 
+        return self.encoder.transform(X).reshape(-1,1)
 
 class DatasetTransformer():
     def __init__(self, X_tfms):
         self.X_numerical = []
         self.X_categorical = []
         
+        numerical_index = 0
+        categorical_index = 0
+        
         for X_tfm in X_tfms:
-            _, tfm = X_tfm
+            key, tfm = X_tfm
+            entry = {'key' : key, 'tfm' : tfm}
 
             if isinstance(tfm, CategoricalTransformer):
-                self.X_categorical.append(tfm)
+                entry['index'] = categorical_index
+                self.X_categorical.append(entry)
+                categorical_index += 1
                 
             if isinstance(tfm, NumericalTransformer):
-                self.X_numerical.append(tfm)
+                entry['index'] = numerical_index
+                self.X_numerical.append(entry)
+                numerical_index += 1
                 
     def __reshape_X(self, X):
         X_transformed = {}
-
-        for tfm in [tfm for tfm in self.X_categorical if tfm.key is not None]:
-            X_transformed[tfm.key] = [x[tfm.key] for x in X]
-        
-        for tfm in [tfm for tfm in self.X_numerical if tfm.key is not None]:
-            X_transformed[tfm.key] = [x[tfm.key] for x in X]
+        keys = X[0].keys()
+        for key in keys:
+            X_transformed[key] = [x[key] for x in X]
             
         return X_transformed
 
@@ -154,101 +160,55 @@ class DatasetTransformer():
         numerical_index = 0
         categorical_index = 0
         
-        for tfm in self.X_categorical:
+        for tfm in [entry['tfm'] for entry in self.X_categorical]:
             categorical_features[:,categorical_index] = tfm.fit_transform(X)
             categorical_index += 1
-            
-        #print(categorical_features)
                
-        for tfm in self.X_numerical:
+        for tfm in [entry['tfm'] for entry in self.X_numerical]:
             numerical_features[:,numerical_index] = tfm.fit_transform(X)
             numerical_index += 1
                
         return numerical_features, categorical_features
     
-    def transform(self, features:List[Dict[str, any]]) -> np.ndarray:
-        dataset_len = len(features)
+    def transform(self, X, Y=None):
+        dataset_len = len(X)
+        X = self.__reshape_X(X)
         
-        numerical_features = np.zeros((dataset_len, len(self.numerical_features)), dtype=np.float64)
-        categorical_features = np.zeros((dataset_len, len(self.categorical_features)), dtype=np.int64)
+        numerical_features = np.zeros((dataset_len, len(self.X_numerical)), dtype=np.float64)
+        categorical_features = np.zeros((dataset_len, len(self.X_categorical)), dtype=np.int64)
         
         numerical_index = 0
         categorical_index = 0
         
-        for feature, tms in self.categorical_features:
-            _features = tms.feature
-            data = np.array([record[_features] for record in features]) 
-            
-            categorical_features[:,categorical_index] = tms.transform(data)
+        for tfm in [entry['tfm'] for entry in self.X_categorical]:
+            categorical_features[:,categorical_index] = tfm.transform(X)
             categorical_index += 1
-                
-        for feature, tms in self.numerical_features:
-            _features = tms.feature
-            
-            if isinstance(_features, str):
-                data = np.array([record[_features] for record in features])
-            else:
-                data_list = []
-                for __feature in _features:
-                    data_list.append(np.array([record[__feature] for record in features]))
-                    
-                data = np.vstack(tuple(data_list))
-            
-            numerical_features[:,numerical_index] = tms.transform(data)
+               
+        for tfm in [entry['tfm'] for entry in self.X_numerical]:
+            numerical_features[:,numerical_index] = tfm.transform(X)
             numerical_index += 1
-
+               
         return numerical_features, categorical_features
-    
-    def get(self, feature:str) -> Tuple[int, str, TransformerMixin]:
-        for i, feature_tms in enumerate(self.categorical_features):
-            _feature, tms = feature_tms
-            if _feature == feature:
-                return i, feature, tms
-            
-        for i, feature_tms in enumerate(self.numerical_features):
-            _feature, tms = feature_tms
-            if _feature == feature:
-                return i, feature, tms
-            
-        raise KeyError(f"Feature {feature} doesn't exist")
         
-    def get_embeddings_size(self, embedding_max_size:int=50) -> list:
+    def get_embeddings_size(self, embedding_max_size=50):
         embedding_dims = []
-        for feature_tms in self.categorical_features:
-            _, tms = feature_tms
-
-            q_unique_values = len(tms.encoder.classes_)
+        for tmf in [entry['tfm'] for entry in self.X_categorical]:
+            q_unique_values = len(tmf.encoder.classes_)
             embedding_size = min(q_unique_values//2, embedding_max_size)
             mapping = (q_unique_values, embedding_size)
             embedding_dims.append(mapping)
             
         return embedding_dims
     
-    def get_features_quantity(self) -> tuple:
-        return len(self.numerical_features), len(self.categorical_features)
+    def get_features_quantity(self):
+        return len(self.X_numerical), len(self.X_categorical)
         
-    def dumps(self, filename:str):
+    def dumps(self, filename):
         with open(filename, 'wb') as f:
             return pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
 
     @staticmethod
     def loads(obj):
         return pickle.loads(obj)
-    
-    @staticmethod
-    def delete_features(feature_type:str, features_to_delete:Union[str, List[str]], features:np.ndarray) -> np.ndarray:
-        if dataset_type not in ['numerical', 'categorical']:
-            raise KeyError(f"FeatureType {feature_type} not allowed")
-            
-        features_to_delete = [features_to_delete] if isinstance(features_to_delete, str) else features_to_delete
-        index_to_delete = []
-            
-        for i, feature_tms in enumerate(getattr(self, f'{feature_type}_features')):
-            feature,_ = feature_tms
-            
-            if feature in features_to_delete:
-                index_to_delete.append(i)
-                
-        return np.delete(features, index_to_delete, 1)
                 
             
